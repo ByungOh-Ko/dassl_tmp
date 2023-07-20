4a2c3e311a81a354f951186dacc795ce9f79c2f5
ByungOh-Ko <ko990128@gmail.com>
2023-07-20 11:21:45 +0900

    tmp

--- dassl/data/datasets/base_dataset.py
+++ dassl/data/datasets/base_dataset.py
@@ -45,6 +45,47 @@ class Datum:
         return self._classname
 
 
+class DatumWithBbox:
+    """Data instance which defines the basic attributes.
+
+    Args:
+        impath (str): image path.
+        label (int): class label.
+        domain (int): domain label.
+        classname (str): class name.
+    """
+
+    def __init__(self, impath="", label=0, domain=0, classname="", bbox=[0, 0, 0, 0]):
+        assert isinstance(impath, str)
+        assert check_isfile(impath)
+
+        self._impath = impath
+        self._label = label
+        self._domain = domain
+        self._classname = classname
+        self._bbox = bbox
+
+    @property
+    def impath(self):
+        return self._impath
+
+    @property
+    def label(self):
+        return self._label
+
+    @property
+    def domain(self):
+        return self._domain
+
+    @property
+    def classname(self):
+        return self._classname
+
+    @property
+    def bbox(self):
+        return self._bbox
+
+
 class DatasetBase:
     """A unified dataset class for
     1) domain adaptation
